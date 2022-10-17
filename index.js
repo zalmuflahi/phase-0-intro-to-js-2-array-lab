d4428de9f1188c4aa504cee13e6088527fa39c00
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-17 02:14:10 -0400

    commit tree 5cfbe3e5059b7947ae80350dcec20323283dc7fb

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(name){ 
+    cats.unshift("Bob") 
+}   
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+ const air = [...cats, "Broom"]
+ return air
+}
+function prependCat(name){
+ const air = ["Arnold", ...cats]
+ return air
+}
+function removeLastCat(){
+ const cats =["Milo", "Otis", "Garfield"]
+ cats.pop()
+ return cats
+}
+function removeFirstCat(){
+  const cats = ["Arnold", "Otis", "Garfield"]
+  cats.shift()
+  return cats
+  
+}
